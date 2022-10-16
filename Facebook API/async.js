9050b44f015c93b1065d40f7673dcd5fc0c5df96
Arafat <arafat6462@gmail.com>
2022-10-16 17:47:02 +0600

    fixed video upload

--- Facebook API/async.js
+++ Facebook API/async.js
@@ -12,7 +12,7 @@ async function getData() {
 
 
 async function postData() {
-   let text = document.getElementById('text').value;
+   let title = document.getElementById('text').value;
    let image_link = document.getElementById('image').value;
    let video_link = document.getElementById('video').value;
    let token = document.getElementById('token').value;
@@ -24,29 +24,58 @@ async function postData() {
    let link = "";
    let url = "";
 
-   if (post_type == "text") type = "feed";
+   if (post_type == "text") {
+      type = "feed";
+      title = "?message=" + title;
+   }
    else if (post_type == "image") {
       type = "photos";
-      link = image_link;
+      title = "?message=" + title;
       url = "&url=";
+      link = image_link;
    }
    else if (post_type == "video") {
       type = "videos";
+      title = "?description=" + title;
+      url = "&file_url=";
       link = video_link;
-      url = "&url=";
+
    }
 
 
-   let res = await fetch("https://graph.facebook.com/v15.0/me/" + type + "?message=" + text + url + link + "&access_token=" + token,
+   let res = await fetch("https://graph.facebook.com/v15.0/me/" + type + title + url + link + "&access_token=" + token,
       { method: 'POST' });
 
 
-   // {method: 'POST', body:JSON.stringify(message=post_text.value)});
- 
    let data = await res.text();
    post_status.innerText = "Request status : \n" + data;
 
 }
+
+
+
+function postData2() {
+   FB.api(
+      '/me/videos',
+      'POST',
+      {
+         // message: "vid_2",
+         file_url: "https://assets.mixkit.co/videos/preview/mixkit-people-pouring-a-warm-drink-around-a-campfire-513-large.mp4",
+         access_token: " ",
+         title: "this is a Title",
+         description: "this is a description"
+      },
+      function (response) {
+         // Insert your code here
+         console.log(response);
+      }
+   );
+}
+
+
+// https://developers.facebook.com/docs/video-api/guides/publishing/
+// including either the {source} parameter (for local video files) or {file_url} parameter (for files hosted on a public server)
+
 
 
 
